1d0abfbd5b7e76b1e1ca1843a81c9d0443e98755
Abdullah <abdullah.5121011@gmail.com>
2021-04-07 17:54:46 -0600

    Added wave display

---
+++ js/script.js
@@ -0,0 +1,234 @@
+// Modified from 
+//   Source 1 : https://gist.github.com/meziantou/edb7217fddfbb70e899e 
+//   Source 2 : https://css-tricks.com/making-an-audio-waveform-visualizer-with-vanilla-javascript/
+
+var startRecordingButton = document.getElementById("startRecordingButton");
+var stopRecordingButton = document.getElementById("stopRecordingButton");
+var showWave = document.getElementById("showWave");
+
+var leftchannel = [];
+var rightchannel = [];
+var recorder = null;
+var recordingLength = 0;
+var volume = null;
+var mediaStream = null;
+var sampleRate = 44100;
+var context = null;
+var blob = null;
+
+startRecordingButton.addEventListener("click", function () {
+    // Initialize recorder
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || window.webkitAudioContex;
+    navigator.getUserMedia(
+    {
+        audio: true
+    },
+    function (e) {
+        console.log("user consent");
+
+        // creates the audio context
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        context = new AudioContext();
+
+        // creates an audio node from the microphone incoming stream
+        mediaStream = context.createMediaStreamSource(e);
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/AudioContext/createScriptProcessor
+        // bufferSize: the onaudioprocess event is called when the buffer is full
+        var bufferSize = 2048;
+        var numberOfInputChannels = 2;
+        var numberOfOutputChannels = 2;
+        if (context.createScriptProcessor) {
+            recorder = context.createScriptProcessor(bufferSize, numberOfInputChannels, numberOfOutputChannels);
+        } else {
+            recorder = context.createJavaScriptNode(bufferSize, numberOfInputChannels, numberOfOutputChannels);
+        }
+
+        recorder.onaudioprocess = function (e) {
+            leftchannel.push(new Float32Array(e.inputBuffer.getChannelData(0)));
+            rightchannel.push(new Float32Array(e.inputBuffer.getChannelData(1)));
+            recordingLength += bufferSize;
+        }
+
+        // we connect the recorder
+        mediaStream.connect(recorder);
+        recorder.connect(context.destination);
+    },
+                function (e) {
+                    console.error(e);
+                });
+});
+
+stopRecordingButton.addEventListener("click", function () {
+
+    // stop recording
+    recorder.disconnect(context.destination);
+    mediaStream.disconnect(recorder);
+
+    // we flat the left and right channels down
+    // Float32Array[] => Float32Array
+    var leftBuffer = flattenArray(leftchannel, recordingLength);
+    var rightBuffer = flattenArray(rightchannel, recordingLength);
+    // we interleave both channels together
+    // [left[0],right[0],left[1],right[1],...]
+    var interleaved = interleave(leftBuffer, rightBuffer);
+
+    // we create our wav file
+    var buffer = new ArrayBuffer(44 + interleaved.length * 2);
+    var view = new DataView(buffer);
+
+    // RIFF chunk descriptor
+    writeUTFBytes(view, 0, 'RIFF');
+    view.setUint32(4, 44 + interleaved.length * 2, true);
+    writeUTFBytes(view, 8, 'WAVE');
+    // FMT sub-chunk
+    writeUTFBytes(view, 12, 'fmt ');
+    view.setUint32(16, 16, true); // chunkSize
+    view.setUint16(20, 1, true); // wFormatTag
+    view.setUint16(22, 2, true); // wChannels: stereo (2 channels)
+    view.setUint32(24, sampleRate, true); // dwSamplesPerSec
+    view.setUint32(28, sampleRate * 4, true); // dwAvgBytesPerSec
+    view.setUint16(32, 4, true); // wBlockAlign
+    view.setUint16(34, 16, true); // wBitsPerSample
+    // data sub-chunk
+    writeUTFBytes(view, 36, 'data');
+    view.setUint32(40, interleaved.length * 2, true);
+
+    // write the PCM samples
+    var index = 44;
+    var volume = 1;
+    for (var i = 0; i < interleaved.length; i++) {
+        view.setInt16(index, interleaved[i] * (0x7FFF * volume), true);
+        index += 2;
+    }
+
+    // our final blob
+    blob = new Blob([view], { type: 'audio/wav' });
+    // saveAs(blob, '1.mp3')
+});
+
+showWave.addEventListener("click", function () {
+    url = URL.createObjectURL(blob);
+    drawAudio(url);
+});
+
+const drawAudio = url => {
+    var audioContext = new AudioContext();
+    fetch(url)
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => draw(normalizeData(filterData(audioBuffer))));
+};
+
+/**
+ * Filters the AudioBuffer retrieved from an external source
+ * @param {AudioBuffer} audioBuffer the AudioBuffer from drawAudio()
+ * @returns {Array} an array of floating point numbers
+ */
+const filterData = audioBuffer => {
+  const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
+  const samples = 70; // Number of samples we want to have in our final data set
+  const blockSize = Math.floor(rawData.length / samples); // the number of samples in each subdivision
+  const filteredData = [];
+  for (let i = 0; i < samples; i++) {
+    let blockStart = blockSize * i; // the location of the first sample in the block
+    let sum = 0;
+    for (let j = 0; j < blockSize; j++) {
+      sum = sum + Math.abs(rawData[blockStart + j]); // find the sum of all the samples in the block
+    }
+    filteredData.push(sum / blockSize); // divide the sum by the block size to get the average
+  }
+  return filteredData;
+};
+
+/**
+ * Normalizes the audio data to make a cleaner illustration 
+ * @param {Array} filteredData the data from filterData()
+ * @returns {Array} an normalized array of floating point numbers
+ */
+const normalizeData = filteredData => {
+    const multiplier = Math.pow(Math.max(...filteredData), -1);
+    return filteredData.map(n => n * multiplier);
+}
+
+/**
+ * Draws the audio file into a canvas element.
+ * @param {Array} normalizedData The filtered array returned from filterData()
+ * @returns {Array} a normalized array of data
+ */
+const draw = normalizedData => {
+  // console.log(normalizedData);
+  // set up the canvas
+  const canvas = document.querySelector("canvas");
+  const dpr = window.devicePixelRatio || 1;
+  const padding = 20;
+  canvas.width = canvas.offsetWidth * dpr;
+  canvas.height = (canvas.offsetHeight + padding * 2) * dpr;
+  const ctx = canvas.getContext("2d");
+  ctx.scale(dpr, dpr);
+  ctx.translate(0, canvas.offsetHeight / 2 + padding); // set Y = 0 to be in the middle of the canvas
+
+  // draw the line segments
+  const width = canvas.offsetWidth / normalizedData.length;
+  for (let i = 0; i < normalizedData.length; i++) {
+    const x = width * i;
+    let height = normalizedData[i] * canvas.offsetHeight - padding;
+    if (height < 0) {
+        height = 0;
+    } else if (height > canvas.offsetHeight / 2) {
+        height = height > canvas.offsetHeight / 2;
+    }
+    drawLineSegment(ctx, x, height, width, (i + 1) % 2);
+  }
+};
+
+/**
+ * A utility function for drawing our line segments
+ * @param {AudioContext} ctx the audio context 
+ * @param {number} x  the x coordinate of the beginning of the line segment
+ * @param {number} height the desired height of the line segment
+ * @param {number} width the desired width of the line segment
+ * @param {boolean} isEven whether or not the segmented is even-numbered
+ */
+const drawLineSegment = (ctx, x, height, width, isEven) => {
+  ctx.lineWidth = 1; // how thick the line is
+  ctx.strokeStyle = "#666"; // what color our line is
+  ctx.beginPath();
+  height = isEven ? height : -height;
+  ctx.moveTo(x, 0);
+  ctx.lineTo(x, height);
+  ctx.arc(x + width / 2, height, width / 2, Math.PI, 0, isEven);
+  ctx.lineTo(x + width, 0);
+  ctx.stroke();
+};
+
+function flattenArray(channelBuffer, recordingLength) {
+    var result = new Float32Array(recordingLength);
+    var offset = 0;
+    for (var i = 0; i < channelBuffer.length; i++) {
+        var buffer = channelBuffer[i];
+        result.set(buffer, offset);
+        offset += buffer.length;
+    }
+    return result;
+}
+
+function interleave(leftChannel, rightChannel) {
+    var length = leftChannel.length + rightChannel.length;
+    var result = new Float32Array(length);
+
+    var inputIndex = 0;
+
+    for (var index = 0; index < length;) {
+        result[index++] = leftChannel[inputIndex];
+        result[index++] = rightChannel[inputIndex];
+        inputIndex++;
+    }
+    return result;
+}
+
+function writeUTFBytes(view, offset, string) {
+    for (var i = 0; i < string.length; i++) {
+        view.setUint8(offset + i, string.charCodeAt(i));
+    }
+}
